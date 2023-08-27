5cbf4e848778dacab2d1745f5df283022277d192
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-27 17:50:23 +0530

    cart

--- catalogue.sh
+++ catalogue.sh
@@ -24,26 +24,55 @@ VALIDATE(){
     fi 
 }
 
+
+check_node(){
+    yum list install nodejs
+    if  [ $? -eq 0 ]
+    then 
+        echo " node already installed "
+    else 
+        install_node
+
+}
+
+install_node(){
 echo "curl -sL https://rpm.nodesource.com/setup_lts.x | bash"  &>> $LOG
 VALIDATE " curl req"  &>> $LOG
 yum install nodejs -y &>> $LOG
 VALIDATE "node js"  &>> $LOG
+}
 
 
-useradd roboshop
-VALIDATE "ADDED USER ROBOSHOP "  &>> $LOG
-mkdir /app
-VALIDATE " created app "  &>> $LOG
+id roboshop
+if [ $? -ne 0  ]
+then 
+    echo " roboshop not exists... "
+    useradd roboshop 
+    VALIDATE " created roboshop user"
+    
+else 
+    echo "roboshop user exists..."
+fi 
+
+if [ -d $DIR ]
+then 
+    echo "app dir exists"
+else 
+    mkdir /app
+    VALIDATE "  create app dir....  "
+    
+fi 
+
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
 VALIDATE " downloaded source code " &>> $LOG
 cd /app 
-chmod 777 /tmp 
+ 
 unzip /tmp/catalogue.zip
 VALIDATE "ZIPPING FILE."
 cd /app
 npm install &>> $LOG
 VALIDATE " npm installed "
-cp catalogue.txt /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.txt /etc/systemd/system/catalogue.service
 VALIDATE "copied file" 
 sed -i "s/<MONGODB-SERVER-IPADDRESS>/$MONGOD_HOST/g"  /etc/systemd/system/catalogue.service
 VALIDATE " changed mongodb server"
@@ -53,9 +82,9 @@ systemctl enable catalogue &>> $LOG
 VALIDATE " CATALOGUE START " &>> $LOG
 systemctl start catalogue &>> $LOG
 VALIDATE " CATALOGUE START " &>> $LOG
-cp mongo-client /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo-client /etc/yum.repos.d/mongo.repo
 VALIDATE "copied file"
 yum install mongodb-org-shell -y &>> $LOG
 VALIDATE "installed mongod" &>> $LOG
-mongo --host $MONGODB </app/schema/catalogue.js &>> $LOG
+mongo --host $MONG0D_HOST </app/schema/catalogue.js &>> $LOG
 VALIDATE " inserted data into mongod" &>> $LOG
