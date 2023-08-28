5fa74d135acc4522d5af6c43b0298867cf9d8e0e
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-29 00:33:43 +0530

    cart

--- EC2-CREATE.sh
+++ EC2-CREATE.sh
@@ -20,7 +20,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi 
     
-    echo "creating instance... :$i"
-    aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GRP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-
+    IP_ADDRESS=$(echo "creating instance... :$i"
+    aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GRP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]")
+    echo " ip_address of $i...$IP_ADDRESS"
 done
