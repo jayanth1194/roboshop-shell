3f30e30f56ebf9816e12c7f6897e79d41f946a38
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-29 00:00:35 +0530

    cart

--- EC2-CREATE.sh
+++ EC2-CREATE.sh
@@ -3,9 +3,24 @@ USER=$(id -u)
 DATE=$(date +%F)
 LOG=/tmp/$0-$DATE.log 
 
+INSTANCE_TYPE=''
+IMAGE_ID='ami-051f7e7f6c2f40dc1'
+SECURITY_GRP='sg-07e78ffac793206c2'
 
-NAME=("mongo" "redis" "mysql" "web" "cart")
+
+
+
+NAME=("mongod" "redis" "mysql" "web" "cart")
 for i in ${NAME[@]}
 do 
-    echo "NAME :$i"
+    if [ $i == "mongod" || $i == "mysql" ]
+    then 
+        INSTANCE_TYPE="t3.micro"
+    else 
+        INSTANCE_TYPE="t2.micro"
+    fi 
+    
+    echo "creating instance... :$i"
+    aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GRP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+
 done
