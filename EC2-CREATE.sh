97f38f13d6ebef56abfa2684be24a6ddb9bf7777
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-29 11:02:40 +0530

    cart

--- EC2-CREATE.sh
+++ EC2-CREATE.sh
@@ -19,8 +19,8 @@ do
     else 
         INSTANCE_TYPE="t2.micro"
     fi 
-    
-    IP_ADDRESS=$(echo "creating instance... :$i"
+    echo "creating instance... :$i"
+    IP_ADDRESS=$(
     aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GRP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"|jq -r '.Instances[0].PrivateIpAddress')
     echo " ip_address of $i...$IP_ADDRESS"
     aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
