aa26aecd30544d27a2d0d0a8a62038b783abbd12
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-29 10:26:50 +0530

    cart

--- EC2-CREATE.sh
+++ EC2-CREATE.sh
@@ -6,7 +6,7 @@ LOG=/tmp/$0-$DATE.log
 INSTANCE_TYPE=
 IMAGE_ID='ami-051f7e7f6c2f40dc1'
 SECURITY_GRP='sg-07e78ffac793206c2'
-
+DOMAIN_NAME=brainchange.online
 
 
 
@@ -23,4 +23,18 @@ do
     IP_ADDRESS=$(echo "creating instance... :$i"
     aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GRP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"|jq -r '.Instances[0].PrivateIpAddress')
     echo " ip_address of $i...$IP_ADDRESS"
+    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+
+
 done
